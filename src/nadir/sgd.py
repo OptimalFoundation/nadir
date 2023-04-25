6e89d366e07ee2c9bf90ba2471de101ac5e71c21
bhavnicksm <bhavnicksm@gmail.com>
2023-04-25 20:43:50 +0530

    Change Momentum in SGD; Adam type -> Polyak type

--- src/nadir/sgd.py
+++ src/nadir/sgd.py
@@ -25,9 +25,54 @@ __all__ = ['SGDConfig', 'SGD']
 @dataclass
 class SGDConfig(BaseConfig):
   lr : float = 1E-3
-
+  momentum : float = 0.0
+  dampening : float = 0.0
+  weight_decay : float = 0.0
 
 class SGD(BaseOptimizer):
-  
   def __init__(self, params, config: SGDConfig = SGDConfig()):
     super().__init__(params, config)
+
+  def init_state(self,
+                 state,
+                 group,
+                 param):
+    state['step'] = 0
+
+    if 1 >= self.config.momentum > 0:
+      state['momentum'] = torch.zeros_like(param, memory_format=torch.preserve_format)
+
+
+  def momentum(self,
+               state, 
+               grad):
+    step = state['step']
+    m = state['momentum']
+    beta = self.config.momentum
+    gamma = self.config.dampening
+
+    m.mul_(beta).add_(grad, alpha=(1 - gamma))
+
+    state['momentum'] = m
+    return m
+
+  def update(self,
+             state: Dict[str, any],
+             group: Dict[str, any],
+             grad:  torch.Tensor,
+             param: torch.Tensor):
+    
+    lr = group['lr']
+    beta = self.config.momentum
+
+    if beta != 0.0:
+      m = self.momentum(state, grad)
+    else:
+      m = grad
+
+    param.data.add_(m, alpha = -1 * lr)
+
+    if self.config.weight_decay > 0:
+      param.data.add_(param.data,
+                      alpha = -1 * lr * self.config.weight_decay)
+    state['step'] += 1
