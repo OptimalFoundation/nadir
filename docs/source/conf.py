5f05f633f3da77fb9996b1d47cec412ad7b03b9f
bhavnicksm <bhavnicksm@gmail.com>
2024-06-08 04:03:18 +0530

    Add docs
    Use Sphinx and Sphinx Book Theme for documentation,
    and add a logo for the project. Also, make
    changes to the gitignore file.

---
+++ docs/source/conf.py
@@ -0,0 +1,40 @@
+# Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+
+project = 'nadir'
+copyright = '2024, Bhavnick Minhas'
+author = 'Bhavnick Minhas'
+release = '0.1.1'
+
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+extensions = []
+
+templates_path = ['_templates']
+exclude_patterns = []
+
+
+
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+
+html_theme = 'sphinx_book_theme'
+html_static_path = ['_static']
+html_theme_options = {
+    "repository_url": "https://github.com/OptimalFoundation/nadir",
+    "use_repository_button": True,
+    "use_issues_button": True,
+    "use_download_button": True,
+    "logo":{
+        "image_light": "../logo.png",
+        "image_dark": "../logo_dark.png",
+    }
+}
+html_title = "Nadir"
+
