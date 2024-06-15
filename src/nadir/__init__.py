1e42009c4f4fc0696c41e205db426beb5b220719
bhavnicksm <bhavnicksm@gmail.com>
2024-06-15 21:00:57 +0530

    [bug] Add Nadam and NAG  to __all__

--- src/nadir/__init__.py
+++ src/nadir/__init__.py
@@ -23,6 +23,8 @@ from .adamax import Adamax, AdamaxConfig
 from .base import BaseOptimizer, BaseConfig
 from .lion import Lion, LionConfig
 from .momentum import Momentum, MomentumConfig
+from .nadam import Nadam, NadamConfig
+from .nag import NAG, NAGConfig
 from .rmsprop import RMSProp, RMSPropConfig
 from .radam import Radam, RadamConfig
 from .sgd import SGD, SGDConfig
